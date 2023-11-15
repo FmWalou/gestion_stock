c649eb8ced9d2b69b2c017987dca1cf50fbc9401
Nabil BOUCHERIFI <nboucherifi@corp.free.fr>
2023-11-15 15:59:31 +0100

    json +

--- back/src/api.ts
+++ back/src/api.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express.Router();
 
 app.get('/articles', (req, res) => {
-    res.json([{ id: 'a1', name: 'Tournevis', price: 2.99, qty: 123 }]);
+    res.json([{ id: '1', name: 'Tournevis', price: 2.99, quantity: 123 }]);
 });
 
 export default app;
