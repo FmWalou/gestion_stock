08f38fb6b544aef40021a273606b2275e6f5297d
Nabil BOUCHERIFI <nboucherifi@corp.free.fr>
2023-11-15 15:57:34 +0100

    using typescript compiling and running

---
+++ back/src/api.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express.Router();
+
+app.get('/articles', (req, res) => {
+    res.json([{ id: 'a1', name: 'Tournevis', price: 2.99, qty: 123 }]);
+});
+
+export default app;
