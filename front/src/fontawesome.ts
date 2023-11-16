0afb9cdfd640368ee1e07ecacd61a5a9cafcc33a
Nabil BOUCHERIFI <nboucherifi@corp.free.fr>
2023-11-16 15:01:10 +0100

    add stock

---
+++ front/src/fontawesome.ts
@@ -0,0 +1,17 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+    faAngleRight,
+    faCircleNotch,
+    faPlus,
+    faRotateRight,
+    faTrashCan
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { type Plugin } from 'vue'
+
+export const fontawsomePlugin: Plugin = {
+    install(app) {
+        library.add(faAngleRight, faRotateRight, faPlus, faTrashCan, faCircleNotch)
+        app.component('font-awesome-icon', FontAwesomeIcon)
+    }
+}
--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -16,6 +16,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/LegaleView.vue')
+    },
+    {
+      path: '/stock',
+      name: 'stock',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../stock/views/StockView.vue')
     }
   ]
 })
